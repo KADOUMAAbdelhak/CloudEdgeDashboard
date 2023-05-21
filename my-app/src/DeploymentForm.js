22d5f44f8ceaa3cf65beee19dc9e7c6a990ccb06
KADOUMA Abdelhak <ha_kadouma@esi.dz>
2023-05-21 22:34:58 +0100

    update deployment form

--- my-app/src/DeploymentForm.js
+++ my-app/src/DeploymentForm.js
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-//import './DeploymentForm.css';
+// import './DeploymentForm.css';
 
 
 
