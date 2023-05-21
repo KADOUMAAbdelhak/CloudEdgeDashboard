4862175ae42e89e3be5af7fcace7f90361d3590e
KADOUMA Abdelhak <ha_kadouma@esi.dz>
2023-05-21 11:39:10 +0100

    Update form

--- my-app/src/Deploymentessay.js
+++ my-app/src/Deploymentessay.js
@@ -42,32 +42,6 @@ const DeploymentForm = () => {
                     <label htmlFor={`microservices[${index}].replicas`}>Number of Replicas</label>
                     <Field name={`microservices[${index}].replicas`} placeholder="Number of Replicas" type="number" />
 
-                    <label htmlFor={`microservices[${index}].cpu`}>CPU</label>
-                    <Field name={`microservices[${index}].cpu`} placeholder="CPU Limit" />
-
-                    <label htmlFor={`microservices[${index}].memory`}>Memory</label>
-                    <Field name={`microservices[${index}].memory`} placeholder="Memory Limit" />
-
-                    <label htmlFor={`microservices[${index}].storageRequest`}>Storage Request</label>
-                    <Field name={`microservices[${index}].storageRequest`} placeholder="Storage Request" />
-
-                    <label htmlFor={`microservices[${index}].containerPort`}>Container Port</label>
-                    <Field name={`microservices[${index}].containerPort`} placeholder="Container Port" />
-
-                    <label htmlFor={`microservices[${index}].servicePort`}>Service Port</label>
-                    <Field name={`microservices[${index}].servicePort`} placeholder="Service Port" />
-
-                    <label htmlFor={`microservices[${index}].serviceType`}>Service Type</label>
-                    <Field as="select" name={`microservices[${index}].serviceType`} placeholder="Service Type">
-                      <option value="ClusterIP">ClusterIP</option>
-                      <option value="NodePort">NodePort</option>
-                      <option value="LoadBalancer">LoadBalancer</option>
-                      <option value="ExternalName">ExternalName</option>
-                    </Field>
-
-                     
-
-
                     {/* Add more fields specific to each microservice */}
                     {/* ... */}
 
